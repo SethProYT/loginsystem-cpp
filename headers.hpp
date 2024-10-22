570b01d0635f3c40d4087d2394ef675edd6d6080
fedorav3 <kasperskyy@fedora.buzz>
2024-10-22 12:30:17 +0000

    Added register function

--- headers.hpp
+++ headers.hpp
@@ -1,11 +1,38 @@
-#ifndef HEADERHPP
-#define HEADERHPP
+#pragma once
 
 #include <iostream>
+#include <string>
+// We will use this later #include <vector>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <filesystem>
 
-void nothing() {
-   std::cout << "Do nothing";
+typedef nlohmann::json json;
+
+// Register function (haha im adding comments to look like ChatGPT that's what you're thinking aren't you?)
+void RegisterAccount(std::string username, std::string password) {
+   std::cout << "Registering account.." << std::endl;
+
+   json NewAccount;
+
+   NewAccount["username"] = username;
+   NewAccount["password"] = password; 
+   
+   // Check if users.json exists
+   std::ofstream check("users.json", std::ios::app);
+
+   if (std::filesystem::exists("users.json")) {
+      std::cout << "Found users.json!" << std::endl;
+      check << NewAccount.dump(4);
+      std::cout << "Wrote to users.json instead of overwritting" << std::endl;
+      return;
+   }
+
+   // New file!!!!
+   std::ofstream json("users.json");
+
+   json << NewAccount.dump(4);
 }
 
 
-#endif
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 #include "headers.hpp"
 
 int main() {
-   nothing();
+   std::cout << "Hello I see you are new enter your username Let's register: ";
+   std::string user;
+   std::cin >> user;
+
+   std::cout << "Now give me your password: ";   
+   std::string pass;
+   std::cin >> pass;
+
+
+   RegisterAccount(user, pass);
 }

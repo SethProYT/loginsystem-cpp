3af08f9379c4e55ad815264a1a22543aee6b4a51
fedorav3 <kasperskyy@fedora.buzz>
2024-10-22 13:13:17 +0000

    Almost there

--- headers.hpp
+++ headers.hpp
@@ -11,28 +11,51 @@ typedef nlohmann::json json;
 
 // Register function (haha im adding comments to look like ChatGPT that's what you're thinking aren't you?)
 void RegisterAccount(std::string username, std::string password) {
-   std::cout << "Registering account.." << std::endl;
+    json NewAccount;
+    NewAccount["username"] = username;
+    NewAccount["password"] = password;
+    
+    if (std::filesystem::exists("users.json")) {
 
-   json NewAccount;
+        std::ifstream read_file("users.json");
+        json all_users = json::parse(read_file);
+        read_file.close();
+        
+        all_users.push_back(NewAccount);
+        
+        std::ofstream write_file("users.json");
+        write_file << all_users.dump(4);
+        std::cout << "Account registered successfully!" << std::endl;
 
-   NewAccount["username"] = username;
-   NewAccount["password"] = password; 
-   
-   // Check if users.json exists
-   std::ofstream check("users.json", std::ios::app);
-
-   if (std::filesystem::exists("users.json")) {
-      std::cout << "Found users.json!" << std::endl;
-      check << NewAccount.dump(4);
-      std::cout << "Wrote to users.json instead of overwritting" << std::endl;
-      return;
-   }
-
-   // New file!!!!
-   std::ofstream json("users.json");
-
-   json << NewAccount.dump(4);
+    } else {
+        std::ofstream json("users.json");
+        json << json::array({NewAccount}).dump(4);
+        std::cout << "Created new users.json with first account!" << std::endl;
+    }
 }
 
 
-
+bool LoginAccount(std::string username, std::string password) {
+    if (std::filesystem::exists("users.json")) {
+        std::ifstream database("users.json");
+        json data = json::parse(database);
+        
+        for (const auto& user : data) {
+            if (user["username"] == username) {
+                if (user["password"] == password) {
+                    std::cout << "Correct password and username" << std::endl;
+                    return true;
+                } else {
+                    std::cout << "Correct user, but wrong password" << std::endl;
+                    return false;
+                }
+            }
+        }
+        
+        std::cout << "User not found. Please register" << std::endl;
+        return false;
+    }
+    
+    std::cout << "No users.json file found!" << std::endl;
+    return false;
+}
--- main.cpp
+++ main.cpp
@@ -3,14 +3,35 @@
 #include "headers.hpp"
 
 int main() {
-   std::cout << "Hello I see you are new enter your username Let's register: ";
-   std::string user;
-   std::cin >> user;
+    std::cout << "Do you have an account? y/n: ";
+    char answer;
+    std::cin >> answer;
+    
+    std::string username;
+    std::string password;
 
-   std::cout << "Now give me your password: ";   
-   std::string pass;
-   std::cin >> pass;
-
-
-   RegisterAccount(user, pass);
+    switch(answer) {
+        case 'n':
+        case 'N':
+            std::cout << "Hello I see you are new enter your username Let's register: ";
+            std::cin >> username;
+            std::cout << "Now give me your password: ";   
+            std::cin >> password;
+            RegisterAccount(username, password);
+            break;
+            
+        case 'y':
+        case 'Y':
+            std::cout << "Username: ";
+            std::cin >> username;
+            std::cout << "Password: ";
+            std::cin >> password;
+            LoginAccount(username, password);
+            break;
+            
+        default:
+            std::cout << "ANSWER Y OR N!!!!" << std::endl;
+            break;
+    }
+    return 0;
 }
